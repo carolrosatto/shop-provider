ad7accdeeafbfafdec08914e7777b0bd02abbc43
carolrosatto <carolrosatto@gmail.com>
2022-04-25 16:27:42 -0300

    Add product form

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:provider_shop/data/products_data.dart';
 import 'package:provider_shop/models/product.dart';
@@ -17,9 +19,44 @@ class ProductList with ChangeNotifier {
     return _items.length;
   }
 
+  saveProductFromData(Map<String, Object> data) {
+    bool hasId = data['id'] != null;
+
+    final newProduct = Product(
+      id: hasId ? data['id'] as String : Random().nextDouble.toString(),
+      name: data['name'] as String,
+      description: data['description'] as String,
+      price: data['price'] as double,
+      imageUrl: data['imageUrl'] as String,
+    );
+    if (hasId) {
+      updateProduct(newProduct);
+    } else {
+      addProduct(newProduct);
+    }
+  }
+
   void addProduct(Product product) {
     _items.add(product);
     notifyListeners();
+  }
+
+  void updateProduct(Product product) {
+    int index = _items.indexWhere((prd) => prd.id == product.id);
+
+    if (index >= 0) {
+      _items[index] = product;
+      notifyListeners();
+    }
+  }
+
+  void deleteProduct(Product product) {
+    int index = _items.indexWhere((prd) => prd.id == product.id);
+
+    if (index >= 0) {
+      _items.removeWhere((prd) => prd.id == product.id);
+      notifyListeners();
+    }
   }
 
   void showFavoritesOnly() {
